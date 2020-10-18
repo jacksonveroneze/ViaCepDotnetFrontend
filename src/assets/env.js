48a29a7405e73b239c4903192fbd140ab286b2d3
Jackson Veroneze <jackson@jacksonveroneze.com>
2020-10-18 17:10:55 -0300

    Ajustes estrutura do projeto.

--- src/assets/env.js
+++
@@ -1,9 +0,0 @@
-(function(window) {
-  window.env = window.env || {};
-
-  // Environment variables
-  window["apiUrl"] = "${API_URL}";
-
-  console.log('__________________');
-  console.log(window["apiUrl"]);
-})(this);
